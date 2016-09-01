c33f5054b6cfabe589c522092f4e6d16d296f62b
mjniday <michael.niday@gmail.com>
2016-08-31 22:48:16 -0400

    Adding API key to headers

--- lib/footy_data.rb
+++ lib/footy_data.rb
@@ -5,8 +5,17 @@ require_relative 'footy_data/team'
 module FootyData
   API_URL = "http://api.football-data.org/v1/"
 
+  def self.api_key=(key)
+    @api_key = key
+  end
+
+  def self.api_key
+    @api_key
+  end
+
   def self.request(url,options)
     request_url = FootyData::API_URL + url + "?" + options.map {|k,v| "#{k}=#{v}"}.join("&")
-    HTTParty.get(request_url).parsed_response
+    headers = :headers => {"X-Auth-Token" => FootyData.api_key}
+    HTTParty.get(request_url,headers).parsed_response
   end
 end
